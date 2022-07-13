8179ccd6214cecfa265c4482e13c571a4e331fb0
Antonyly <antonlyzun@gmail.com>
2022-07-13 20:39:23 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+ // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsEl = createGallery(galleryItems);
+galleryEl.addEventListener('click', galleryEventEl);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsEl);
+function createGallery(el) {
+    return el.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`
+    }).join("");
+}
+
+function galleryEventEl(el) {
+  el.prevent
+};
